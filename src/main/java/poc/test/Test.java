8dd9fd0fdd68730b1f55a4ccb1af3a51ac9ef539
esiva <sivakumar.ekambaram@hmhco.com>
2016-05-04 16:27:42 +0100

    test1

---
+++ src/main/java/poc/test/Test.java
@@ -0,0 +1,14 @@
+package poc.test;
+
+public class Test {
+	public static void main(String[] args) {
+		int x = 0;
+		method1(x++);
+		method1(x++);
+	}
+	
+	private static void method1(int i) {
+		System.out.println(i);
+	}
+
+}
